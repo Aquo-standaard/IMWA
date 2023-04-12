2d4be1c086752e7b8b007a929f13a1ebef5eed30
WijnandIHW <104829403+WijnandIHW@users.noreply.github.com>
2023-04-12 16:19:44 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+var respecConfig = {
+  specStatus: "GN-CV",
+  specType: "IM",
+  //canonicalURI: "WV",
+  //publishDate: "2020-03-26",
+  maxTocLevel: 3,
+   logos: [ {
+    src: 'https://github.com/Aquo-standaard/IMWA/blob/main/media/logo-IHW-200px.png',
+    href: "...",
+    alt: "Informatiehuis Water",
+    width: 776,
+    height: 163,
+    id: 'logo_aquo',
+  }]  ,
+   editors: [    {
+      name: "Informatiehuis Water",	
+      companyURL: "https://www.ihw.nl/aquo-standaard",
+    }],
+  authors: [
+    {   name: "Informatiehuis Water",	
+      companyURL: "https://www.aquo.nl/IMWA",,
+    }
+	],
+  shortName: "IMWA",
+  pubDomain: "---",
+  github: "https://github.com/Aquo-standaard/IMWA",
+  previousPublishDate: "2023-04-12",
+  previousStatus: "GN-DEF",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
